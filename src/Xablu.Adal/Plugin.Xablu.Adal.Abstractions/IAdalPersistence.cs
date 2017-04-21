17a24de172df39603c78ebffd3abf7e67f2f3e17
Edwin van der Ham <e.vanderham@xablu.com>
2017-04-21 16:38:50 +0200

    using plugin template for initial commit

---
+++ src/Xablu.Adal/Plugin.Xablu.Adal.Abstractions/IAdalPersistence.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Plugin.Xablu.Adal.Abstractions
+{
+    public interface IAdalPersistence
+    {
+        Task<byte[]> RetrieveTokenCache();
+        Task PersistTokenCache(byte[] data);
+        Task RemoveTokenCache();
+
+        Task<string> RetrieveUserId();
+        Task PersistUserId(string userId);
+        Task RemoveUserId();
+
+    }
+}
---
+++ src/Xablu.Adal/Plugin.Xablu.Adal.Abstractions/InMemoryAdalPersistence.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+
+namespace Plugin.Xablu.Adal.Abstractions
+{
+    internal class InMemoryAdalPersistence : IAdalPersistence
+    {
+        private string _userId;
+        private byte[] _data;
+
+        public Task PersistTokenCache(byte[] data)
+        {
+            _data = data;
+            return Task.FromResult(0);
+        }
+
+        public Task PersistUserId(string userId)
+        {
+            _userId = userId;
+            return Task.FromResult(0);
+        }
+
+        public Task<byte[]> RetrieveTokenCache()
+        {
+            return Task.FromResult(_data);
+        }
+
+        public Task<string> RetrieveUserId()
+        {
+            return Task.FromResult(_userId);
+        }
+
+        public Task RemoveTokenCache()
+        {
+            _data = null;
+            return Task.FromResult(0);
+        }
+
+        public Task RemoveUserId()
+        {
+            _userId = null;
+            return Task.FromResult(0);
+        }
+    }
+}
