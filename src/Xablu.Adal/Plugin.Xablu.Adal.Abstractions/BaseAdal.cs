1f06d02bdc7db8c2d55e184ce109a1b92d4becc7
Marc Bruins <marc@marcbruins.nl>
2017-07-19 15:18:54 +0200

    Add ability to refresh the logged in user

--- src/Xablu.Adal/Plugin.Xablu.Adal.Abstractions/BaseAdal.cs
+++ src/Xablu.Adal/Plugin.Xablu.Adal.Abstractions/BaseAdal.cs
@@ -31,9 +31,14 @@ namespace Plugin.Xablu.Adal.Abstractions
             return loggedInUser ?? await GetOrSignInUserAsync(allowUserInteraction: true);
         }
 
-        public async Task<ActiveDirectoryUser> GetLoggedInUserAsync()
+        public async Task<ActiveDirectoryUser> GetLoggedInUserAsync(bool forceRefresh = false)
         {
-            if (loggedInUser == null) await GetOrSignInUserAsync(allowUserInteraction: false);
+            if (loggedInUser == null)
+                await GetOrSignInUserAsync(allowUserInteraction: false);
+
+            if (forceRefresh)
+                loggedInUser = await GetOrSignInUserAsync(allowUserInteraction: false);
+
             return loggedInUser;
         }
 
